17c14a04d0dbc22d2fed9ea916223e58c5ddf393
Muhammad Ayan <mohd.ayanasim.786@gmail.com>
2025-12-03 23:27:14 +0200

    Introduction of Variables.

---
+++ 01-Pure_JS_Concepts/01_Variables.js
@@ -0,0 +1,14 @@
+/* Variables in JavaScript */
+
+// Declaring variables using var, let, and const
+
+console.log("Writing the code for Variables.js");
+
+let a = 60;
+console.log(a);
+
+a = "Hello, World!";
+console.log(a); // Output: Hello, World!
+
+
+// let 8John = "Invalid"; // Invalid variable name, cannot start with a number
