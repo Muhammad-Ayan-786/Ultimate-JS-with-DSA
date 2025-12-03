eb998e9b77ca00ab6519ba830f9ef2a4cb43ce9a
Muhammad Ayan <mohd.ayanasim.786@gmail.com>
2025-12-04 01:42:59 +0200

    Write Strings in JS.

---
+++ 01-Pure_JS_Concepts/09_String.js
@@ -0,0 +1,25 @@
+let str1 = 'Ayan';
+console.log(str1.length);
+
+let str2 = "Harry";
+console.log(str2[0]);
+console.log(str2[1]);
+
+console.log(str2);
+
+// Template Literals
+let boy1 = "John";
+let boy2 = "Prince";
+
+let sectence = `${boy1} is a friend of ${boy2}`
+console.log(sectence);
+
+// Escape Sequence Characters
+let fruit = 'Bana\'na'; // same for "" as well.
+console.log(fruit);
+
+/*
+\n - Newline
+\t - Tab
+\r - Carriage Return
+*/
