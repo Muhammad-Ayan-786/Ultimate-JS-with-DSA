4f7dca9b0cb9ecb84ae1b1b799ad29211948c002
Muhammad Ayan <mohd.ayanasim.786@gmail.com>
2025-12-04 14:08:47 +0200

    Write the code of the first Piller in OOPs, (Encapsulation).

--- 04-OOPs_in_JavaScript/06_Encapsulation.js
+++ 04-OOPs_in_JavaScript/06_Encapsulation.js
@@ -39,3 +39,64 @@ console.log(s.getGrade()); // A
 s.setMarks(50);
 console.log(s.getMarks()); // 50
 console.log(s.getGrade()); // C
+
+console.log(`\n\n`);
+
+// Practice Quiz.
+class Phone {
+    // Private.
+    #Battery;
+    #PIN
+
+    // Public.
+    brand;
+    model;
+
+    // Constructor.
+    constructor(brand, model, battery, pin) {
+        this.brand = brand;
+        this.model = model;
+        this.#validateBattery(battery);
+        this.#validatePIN(pin);
+    }
+
+    // Private Methods ...
+    #validateBattery (battery) {
+        if (battery >= 100)
+            this.#Battery = 100;
+        else if (battery < 0)
+            this.#Battery = 0;
+        else
+            this.#Battery = battery;
+    }
+
+    #validatePIN (pin) {
+        if (String(pin).length != 4)
+            console.error("Invalid PIN !");
+        else
+            this.#PIN = pin;
+    }
+
+    // Getter
+    get batteryStatus () {
+        return `Battery: ${this.#Battery}%`
+    }
+
+    // Setter
+    set updatePin (pin) {
+        if (String(pin).length != 4) {
+            console.log("Invalid PIN !");
+        }
+        else {
+            this.#PIN = pin;
+            console.log("PIN updated successfully");
+        }
+    }
+}
+
+const p = new Phone("Apple", "iPhone 15", 100, 9999); // Object
+
+console.log(p.batteryStatus);    // Battery: 100%
+p.updatePin = 1234;              // valid
+p.updatePin = 77;                // ERROR
+console.log(p.batteryStatus);
