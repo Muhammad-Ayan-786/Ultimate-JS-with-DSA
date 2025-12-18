31e43fba7f496e98d42b66bab1134d6d51f15734
Muhammad Ayan <mohd.ayanasim.786@gmail.com>
2025-12-18 14:13:14 +0200

    Add some code of Window, DOM & BOM.

---
+++ 02-DOM_Manipulation_with_JS/04-Window_BOM_DOM/script.js
@@ -0,0 +1,9 @@
+window.console.log(window);
+
+console.log(document); // HTML Document
+console.log(document.title); // HTML Title
+console.log(document.head); // HTML Head
+console.log(document.body); // HTML Body
+console.log(document.documentElement); // HTML Document Element
+
+document.body.style.background = "linear-gradient(to right, #0f2027, #203a43, #2c5364)";
