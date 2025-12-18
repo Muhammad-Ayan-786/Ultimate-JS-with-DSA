2d2362219873aa00e6a97c155e3798265d29fa88
Muhammad Ayan <mohd.ayanasim.786@gmail.com>
2025-12-18 17:07:22 +0200

    Add code to access elem's Parent or other Siblings

---
+++ 02-DOM_Manipulation_with_JS/07-Parent_and_Siblings_of_Elem/script.js
@@ -0,0 +1,39 @@
+/*
+What's $0 on console browser ?
+
+• $0 in the console refers to the currently selected element in the DOM.
+• $1 refers to the previous selected element in the DOM.
+*/
+
+/* ____________________________
+Siblings and Parent of Element :
+
+Siblings -> Elements that share a common parent are called siblings
+
+• For example -> <head> & <body> are siblings. Siblings share the same parent. In the above example its parent is <html>.
+
+• <body> is said to be the "next" or "right" sibling of <head>. And <head> is said to be the "previous" or "left" sibling of <body>.
+
+• nextSibling -> Returns the next sibling node of an element.
+
+• previousSibling -> Returns the previous sibling node of an element.
+
+
+Parent node -> The element that contains the current element is called its parent
+
+• parentNode -> If you want to access the parent node of an element, you can use the parentNode property.
+
+• parentElement -> If you want to access the parent element of an element, you can use the parentElement property.
+______________________________
+*/
+
+console.log(document.body.firstChild);
+const a = document.body.firstChild;
+
+console.log(a.parentNode); // Returns the parent node of an element
+console.log(a.parentElement); // Returns the parent element of an element
+
+console.log(a.firstChild.nextSibling); // Returns the next sibling node of an element
+
+console.log(document.documentElement.parentNode);
+console.log(document.documentElement.parentElement);
